d596efee024476137d2f0dbc7f3b7a6076242e73
comradeMauser <singularquantumamoeba@gmail.com>
2021-02-09 13:19:18 +0300

    2.1 - 2.6: introduction; using types: num, str, bool

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log('it`s alive!');
-console.log(Number(3) + String([3]));
+"use strict";
+function sum(n, m, k, j) {
+    var result = n + m;
+    return bool ? j + result : 'no result';
+}
+var num1;
+num1 = 10;
+var num2 = 3.3;
+var bool = true;
+var str = "Result: ";
+var run = sum(num1, num2, bool, str);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,2 +1,13 @@
-console.log('it`s alive!')
-console.log(Number(3)+String([3]))
+function sum(n: number, m: number, k: boolean, j: string) {
+    const result = n + m
+    return bool ? j + result : 'no result'
+}
+
+let num1: number
+num1 = 10
+const num2 = 3.3
+const bool = true
+const str = "Result: "
+
+const run = sum(num1, num2, bool, str)
+console.log(run)
