33a79aa392a80354ad6fdc43a4361366967261fe
comradeMauser <singularquantumamoeba@gmail.com>
2021-02-10 12:40:05 +0300

    2.11 - 2.13: type tuple; type enum; type any

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 "use strict";
-// const person: {
-//     firstName: string,
-//     lastName: string,
-//     age: number,
+// const types: {
+//     string: string,
+//     number: number,
+//     arrType: string[],
+//     tuple: [number, string, boolean]
 // } = {
-var person = {
-    firstName: 'Hans',
-    lastName: 'Schmulke',
-    age: 30,
-    strings: ['one', 'two', 'three']
+var Enum;
+(function (Enum) {
+    Enum[Enum["FOUR"] = 0] = "FOUR";
+    Enum[Enum["FIVE"] = 1] = "FIVE";
+    Enum[Enum["SIX"] = 2] = "SIX";
+})(Enum || (Enum = {}));
+var types = {
+    string: 'Name',
+    number: 22,
+    arrType: ['one', 'two', 'three'],
+    tuple: [1, 'a', false],
+    enum: Enum.FIVE
 };
-for (var _i = 0, _a = person.strings; _i < _a.length; _i++) {
-    var str = _a[_i];
-    console.log(str.toUpperCase());
+types.tuple.push('3', 4);
+types.tuple[0] = 2;
+console.log(types.tuple);
+if (types.enum === Enum.FIVE) {
+    console.log(Enum.FIVE);
 }
--- app.ts
+++ app.ts
@@ -1,12 +1,23 @@
-// const person: {
-//     firstName: string,
-//     age: number,
+// const types: {
+//     string: string,
+//     number: number,
+//     arrType: string[],
+//     tuple: [number, string, boolean]
 // } = {
-const person = {
-    firstName: 'Name',
-    age: 22,
-    strings: ['one', 'two', 'three']
+
+enum Enum {FOUR, FIVE, SIX }
+
+const types = {
+    string: 'Name',
+    number: 22,
+    arrType: ['one', 'two', 'three'],
+    tuple: [1, 'a', false],
+    enum: Enum.FIVE
 }
-for (let str of person.strings){
-    console.log(str.toUpperCase())
+types.tuple.push('3', 4)
+types.tuple[0] = 2
+console.log(types.tuple)
+
+if (types.enum === Enum.FIVE) {
+    console.log(Enum.FIVE)
 }
